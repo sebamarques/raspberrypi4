b1ebf3be569b5472116761b1b8d3992be3bea36c
sebamarques <sebamarquezblanco@gmail.com>
2024-07-10 18:51:09 -0300

    v0.2

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ let lista = document.getElementById("cambios");
 const dias = new Date()
 let boton_componentes = document.getElementById("componentes")
 let boton_ejercicios = document.getElementById("rutinas")
+let boton_deseados = document.getElementById("deseados")
+let boton_dinero = document.getElementById("dinero")
 let componentes = {
         0:"Esp32",1:"Esp8266",2:"Esp8266 01",3:"sensor infrarrojo",
         4:"Null",5:"rele",6:"Buzzers y LDR",7:"Ultrasonicos",
         8:"NULL",9:"NULL",10:"Sensor PIR y pantalla oled 0.96",11:"Servos",
         12:"NULL",13:"NULL",14:"LEDS",15:"Sensor de agua"
     }
-
-
+let deseados = ["Silla gamer","3er brazo","tarjeta grafica","mas contenedores de componentes"]
+let dinero_prex_mama = 75
+let mi_dinero = 200
 let rutinas = [
     //Ejercicios Lunes/Jueves
     ["24 pesas brazos normal","24 pesas brazos como paja","24 pesas brazos antebrazo", "24 pesas brazos antebrazo invertido",
@@ -31,7 +34,7 @@ boton_ejercicios.addEventListener("click",function(){
         let dia_semana = dias.getDay();
         let ejercicios = ``
         const obtener = obtenerRutinas(dia_semana)
-        for(let i =0; i<obtener.length;i++){
+        for(let i =0; i<=obtener.length;i++){
             ejercicios+=`
             <div class="ejercicios">
                 <p>${obtener[i]}</p>
@@ -42,18 +45,37 @@ boton_ejercicios.addEventListener("click",function(){
 })
 boton_componentes.addEventListener("click",function(){
     nuevos_componentes = Object.values(componentes)
-    console.log(nuevos_componentes)
-    let guardar_valor = ``;
-    for(let i = 0; i<nuevos_componentes.length;i++){
-        guardar_valor += `
+    let guardar_componentes = ``;
+    for(let i = 0; i<=nuevos_componentes.length;i++){
+        guardar_componentes += `
         <div class="componentes">
             <p>${nuevos_componentes[i]}</p>
         </div>
             `
     }
-    lista.innerHTML = guardar_valor
+    lista.innerHTML = guardar_componentes
 
 })
+boton_deseados.addEventListener("click",function(){
+    guardar_deseados = ``
+    for(let i = 0;i<=deseados.length;i++){
+        guardar_deseados += `
+        <div>
+            <p>${deseados[i]}</p>
+        </div>
+            `
+    }
+    lista.innerHTML = guardar_deseados
+})
+boton_dinero.addEventListener("click",function(){
+    let guardar_dinero = `
+        <div>Dinero billetera: ${mi_dinero} pesos</div>
+        <div>Dinero en prex de Mamá: ${dinero_prex_mama} pesos</div>
+    `
+    lista.innerHTML = guardar_dinero
+
+})
+
 function obtenerRutinas(dia){
     switch(dia){
         case 1:
